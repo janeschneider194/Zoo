e053acd294a42abb6b7e7bf76327d7eebcde5965
msharaf19 <42978512+msharaf19@users.noreply.github.com>
2019-06-03 09:14:04 -0400

    Add files via upload

--- src/Chameleon.java
+++ src/Chameleon.java
@@ -1,17 +1,41 @@
+import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-public class Chameleon extends JPanel
+import javax.swing.JLabel;
+
+public class Chameleon extends JLabel
 {
-	private JFrame fram;
-	public Chameleon(JFrame frame)
+	public Chameleon() 
 	{
-		fram = frame;
 		setLayout(new GridBagLayout());
 		GridBagConstraints gbc = new GridBagConstraints();
 		gbc.gridx = 0;
 		gbc.gridy = 0;
+		
+		JLabel nameLabel = new JLabel("Chameleon");
+		nameLabel.setPreferredSize(new Dimension(100,50));
+		add(nameLabel,gbc);
+		
+		gbc.gridy = 1;
+		
+		JLabel habitatLabel = new JLabel("Habitat: Ranges from rain forests to desert conditions, "
+				+ "with various species occurring in Africa, Madagascar, southern Europe, "
+				+ "and across southern Asia as far as Sri Lanka.");
+		habitatLabel.setPreferredSize(new Dimension(700,50));
+		add(habitatLabel,gbc);
+		
+		gbc.gridy = 2;
+		
+		JLabel dietLabel = new JLabel("Diet: A variety of insects, including gut-loaded (recently fed) crickets, roaches, mealworms and waxworms.");
+		dietLabel.setPreferredSize(new Dimension(500,50));
+		add(dietLabel,gbc);
+		
+		gbc.gridy = 3;
+		
+		JLabel funFactLabel = new JLabel("Fun Fact: Biggest chameleon is Furcifer oustaleti, which reaches 30 inches in length. ");
+		funFactLabel.setPreferredSize(new Dimension(400,50));
+		add(funFactLabel,gbc);
+		
 	}
 }
